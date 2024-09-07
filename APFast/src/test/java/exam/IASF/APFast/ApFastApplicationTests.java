62028bfae3f50cd0c88f2f5764385bb6b17e8180
Đồng Vũ <Dongvu7173@gmail.com>
2024-09-07 08:55:35 +0700

    first commit

---
+++ APFast/src/test/java/exam/IASF/APFast/ApFastApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.IASF.APFast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApFastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
